05dfbf6e80003917eaccb7fe0856132b00f26191
Griffin Pope <griffinpope@byui.edu>
2022-04-26 00:23:17 -0600

    Added week 2 exercises

---
+++ week02/w02.js
@@ -0,0 +1,62 @@
+function chapter02Exercise() {
+  const question = "What is Superman's real name?";
+  const answer = prompt(question);
+  alert(`You answered ${answer}`);
+}
+
+const quiz = [
+  ["What is Superman's real name?", 'Clark Kent'],
+  ["What is Wonder Woman's real name?", 'Diana Prince'],
+  ["What is Batman's real name?", 'Bruce Wayne'],
+];
+
+let score = 0 // initialize score
+
+function chapter03Exercise() {
+
+  // Question: What happens if there are three elements in the array,
+  // and I just deconstruct two of them?
+  for (const [question, answer] of quiz) {
+    const response = prompt(question);
+    if (response === answer) {
+      alert('Correct!');
+      score++;
+    } else {
+      alert(`Wrong! The correct answer was ${answer}`);
+    }
+  }
+
+  // At the end of the game, report the player's score
+  alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+}
+
+function start(quiz) {
+  let score = 0;
+
+  // main game loop
+  for (const [question, answer] of quiz) {
+    const response = ask(question);
+    check(response, answer);
+  }
+  // end of main game loop
+
+  gameOver();
+
+  // function declarations
+  function ask(question) {
+    return prompt(question);
+  }
+
+  function check(response, answer) {
+    if (response === answer) {
+      alert('Correct!');
+      score++;
+    } else {
+      alert(`Wrong! The correct answer was ${answer}`);
+    }
+  }
+
+  function gameOver() {
+    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+  }
+}
